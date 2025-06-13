f7854cb6353638e0b90997b1cc6978997fa05080
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2025-06-13 14:18:33 +0200

    3:a Försöket

--- uppg7.js
+++ uppg7.js
@@ -1,11 +1,11 @@
 
 
-function uppg7(num, num2) {
+function uppg7(num1, num2) {
    
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
-    let sum = num + num2;
+    let sum = num1 + num2;
     
     return console.log(sum);
 }
